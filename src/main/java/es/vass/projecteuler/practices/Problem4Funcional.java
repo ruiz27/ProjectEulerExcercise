95146a04cbb72d59f6653f3daa8419a9f59a4635
sruizvass <sergio.ruiz@vass.es>
2020-06-12 13:14:17 +0200

    Change the operation between list

--- src/main/java/es/vass/projecteuler/practices/Problem4Funcional.java
+++ src/main/java/es/vass/projecteuler/practices/Problem4Funcional.java
@@ -14,9 +14,11 @@ public class Problem4Funcional {
 		List<Integer> list1 = IntStream.range(100, 999).map(x -> x+1).boxed().collect(Collectors.toList());
 		List<Integer> list2 = IntStream.range(100, 999).map(x -> x+1).boxed().collect(Collectors.toList());
 		
-		List<Integer> result = IntStream.range(list1.size(), list2.size()).map(i -> list1.get(i) * list2.get(i)).boxed()
-				.collect(Collectors.toList());
+		List<Integer> result = list1.stream()
+		        .flatMap( s1 -> list2.stream().map( s2 -> s1 * s2 ) )
+		        .collect( Collectors.toList() );
 		;
+		
 		System.out.println(result.stream().filter(isPalindromic).collect(Collectors.toList()));
 	}
 
