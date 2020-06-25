36cf221cc654a61443580b225261279105b9f8f3
sruizvass <sergio.ruiz@vass.es>
2020-06-25 20:24:32 +0200

    Excercise 7

---
+++ src/main/java/es/vass/projecteuler/practices/Problem7.java
@@ -0,0 +1,5 @@
+package es.vass.projecteuler.practices;
+
+public class Problem7 {
+
+}
---
+++ src/test/java/es/vass/projecteuler/practices/Problem7Test.java
@@ -0,0 +1,13 @@
+package es.vass.projecteuler.practices;
+
+import org.junit.Test;
+
+public class Problem7Test {
+
+	@Test
+	public void testPrime() {
+		Problem7Funcional problem = new Problem7Funcional();
+		System.out.println(problem.doExcercise(6));
+	
+	}
+}
